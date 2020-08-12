f85c0f6db324a6f69a2bcfe2961f2d8b6e9d2ddc
John Brunton <john_brunton@live.co.uk>
2020-08-12 11:28:22 +0100

    add tabs to MainActivity

--- example/android/app/src/main/java/com/example/pockettimelinereactmodule/MainActivity.java
+++ example/android/app/src/main/java/com/example/pockettimelinereactmodule/MainActivity.java
@@ -1,15 +1,27 @@
 package com.example.pockettimelinereactmodule;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.facebook.react.ReactActivity;
 
-public class MainActivity extends ReactActivity {
+public class MainActivity extends AppCompatActivity {
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
+//  @Override
+//  protected String getMainComponentName() {
+//    return "PocketTimelineReactModuleExample";
+//  }
+
+
   @Override
-  protected String getMainComponentName() {
-    return "PocketTimelineReactModuleExample";
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
   }
 }
