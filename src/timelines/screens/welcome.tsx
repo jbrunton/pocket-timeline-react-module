ecb6f70df135d74185694d71b872c364354acfb1
John Brunton <john_brunton@live.co.uk>
2020-08-12 10:55:10 +0100

    center text

--- src/timelines/screens/welcome.tsx
+++ src/timelines/screens/welcome.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 export const WelcomeScreen = () => {
-  return <Text>Hello, World!</Text>;
+  return  <View style={{flex:1,justifyContent: "center",alignItems: "center"}}>
+    <Text style={{textAlignVertical: "center",textAlign: "center",}}>Hello, World!</Text>
+  </View>;
 }
