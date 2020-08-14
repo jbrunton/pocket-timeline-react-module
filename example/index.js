9abb6aa8feb2cc8d7aa56095d9df15eba75ed3d9
John Brunton <john_brunton@live.co.uk>
2020-08-14 15:13:28 +0100

    add navigator

--- example/index.js
+++ example/index.js
@@ -1,8 +1,8 @@
 import { AppRegistry } from 'react-native';
 //import App from './src/App';
 import { name as appName } from './app.json';
-import {WelcomeScreen, TimelinesScreen} from 'pocket-timeline-react-module'
+import {WelcomeScreen, TimelinesApp} from 'pocket-timeline-react-module'
 
 
-AppRegistry.registerComponent('TimelinesApp', () => TimelinesScreen);
+AppRegistry.registerComponent('TimelinesApp', () => TimelinesApp);
 AppRegistry.registerComponent('WelcomeApp', () => WelcomeScreen);
